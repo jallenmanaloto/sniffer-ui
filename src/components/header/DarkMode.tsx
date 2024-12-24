1f5bf2f710ea159cde5c7ce7ac34b9b8adb3bc41
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-24 13:17:20 +0800

    feat: create header

---
+++ src/components/header/DarkMode.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import { MoonStar, Sun } from 'lucide-react';
+
+export default function DarkMode() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedDarkMode = localStorage.getItem('darkMode') === 'true';
+    setIsDarkMode(savedDarkMode);
+    if (savedDarkMode) {
+      document.body.classList.add('dark');
+    }
+
+  }, []);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevState: boolean) => {
+      const newState = !prevState;
+      if (newState) {
+        document.body.classList.add('dark');
+        localStorage.setItem('darkMode', 'true');
+      } else {
+        document.body.classList.remove('dark');
+        localStorage.setItem('darkMode', 'false');
+      }
+
+      return newState;
+    })
+  };
+
+  return (
+    <div className="mode-container">
+      {isDarkMode
+        ? <MoonStar color="#14b8a6" className="cursor-pointer" onClick={toggleDarkMode} />
+        : <Sun className="cursor-pointer" onClick={toggleDarkMode} />
+      }
+    </div>
+  )
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,16 @@
+import DarkMode from "./DarkMode";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="flex items-center justify-between py-4">
+        <h1 className="text-3xl font-bold dark:text-teal-500/75">
+          BaitSniffer
+        </h1>
+        <div>
+          <DarkMode />
+        </div>
+      </div>
+    </header>
+  )
+}
