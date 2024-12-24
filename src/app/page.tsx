24a9aa67ca380cdc5fe618e107c5510281b7874b
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-24 14:12:08 +0800

    style: dynamic padding-x on lg screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Lookup from "@/components/lookup/Lookup";
 export default function Home() {
   return (
     <div className="w-screen h-screen dark:bg-slate-900">
-      <main className="container mx-auto px-8">
+      <main className="container mx-auto px-8 lg:px-36">
         <Header />
         <Lookup />
       </main>
