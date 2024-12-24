ae54960f294041d1828f57db17dc1b6003f5c726
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-24 13:31:30 +0800

    feat: add Lookup component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header"
+import Lookup from "@/components/lookup/Lookup";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen dark:bg-slate-900">
       <main className="container mx-auto px-8">
         <Header />
+        <Lookup />
       </main>
     </div>
   );
