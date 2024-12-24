d808405c817e988ee1a679caccfa82bbee0df6f0
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-24 12:46:58 +0800

    feat: add darkmode and screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,22 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          sm: "100%",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+        },
       },
     },
   },
