e4d28d68bbc3fc3e76b3cec7f89b5d51d8db35c2
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-24 15:34:34 +0800

    feat: add Summary component on analysis

--- src/components/analysis/index.tsx
+++ src/components/analysis/index.tsx
@@ -1,14 +1,19 @@
-import { Findings, Screenshot } from "@/lib/types";
+import { RiskLevel, Screenshot } from "@/lib/types";
 import Screenshots from "./Screenshots";
+import { responseData } from "@/data/mock";
+import Summary from "./Summary";
 
 export default function Analysis() {
   // const { riskLevel, screenshots, topicFinding } = findings;
+  const { findings, databaseMatch } = responseData;
+
   const screenshots: Screenshot[] = [
     { image: "https://ik.imagekit.io/iakg6rt33o/20241224_040959_1735013399676242375?updatedAt=1735013401363" },
     { image: "https://ik.imagekit.io/iakg6rt33o/20241224_040954_1735013394422837181?updatedAt=1735013396657" },
   ]
   return (
-    <div className="py-4">
+    <div className="">
+      <Summary riskLevel={findings.riskLevel as RiskLevel} />
       <Screenshots screenshots={screenshots} />
     </div>
   )
