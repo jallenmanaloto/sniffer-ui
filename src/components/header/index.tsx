1dc56da454d3c10775eba9bdd381e556a66e2e0c
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 17:07:28 +0800

    feat: add link to logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,16 @@
 import DarkMode from "./DarkMode";
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="border-b-2">
       <div className="header-wrapper container mx-auto px-8 lg:px-36">
         <div className="flex items-center justify-between py-4">
-          <h1 className="text-4xl font-bold dark:text-teal-500/75">
-            BaitSniffer
-          </h1>
+          <Link href="/">
+            <h1 className="text-4xl font-bold dark:text-teal-500/75">
+              BaitSniffer
+            </h1>
+          </Link>
           <div>
             <DarkMode />
           </div>
