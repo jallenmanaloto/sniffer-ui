c73e9d04426cde9105ef385222f6ef62dd910f2a
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 14:30:36 +0800

    feat: add data for all topics

--- src/data/mock.ts
+++ src/data/mock.ts
@@ -5,14 +5,14 @@ export const responseData: AnalysisResponse = {
   findings: {
     topicFinding: {
       domain: {
-        points: 2,
+        points: 3,
         reasons: {
           bad: [
-            "This Domain is bad.",
-            "This domain is a scam."
+            "Website uses a common top-level domain that is frequently associated with phishing attacks: ..pro.",
+            "Multiple dashes were detected in the URL. Phishing websites often use an excessive number of dashes in their domain names to appear legitimate."
           ],
           good: [
-            "This domain quite good."
+            "No suspiciously replaced characters found in the URL, which is common in phishing sites trying to mimic legitimate domains."
           ]
         }
       },
@@ -20,29 +20,39 @@ export const responseData: AnalysisResponse = {
         points: 0,
         reasons: {
           bad: null,
-          good: null,
+          good: [
+            "Website has a valid SSL certificate and uses a secure TLS connection, ensuring encrypted communication."
+          ],
         }
       },
       content: {
-        points: 0,
+        points: 3,
         reasons: {
-          bad: null,
+          bad: [
+            "Found suspicious keyword 'phone number' in the page content, which is often associated with phishing attacks.",
+            "Found suspicious keyword 'exchange' in the page content, which is often associated with phishing attacks.",
+            "Found suspicious keyword 'gift' in the page content, which is often associated with phishing attacks.",
+            "Found suspicious keyword 'redeem' in the page content, which is often associated with phishing attacks.",
+            "Found suspicious keyword 'points' in the page content, which is often associated with phishing attacks."
+          ],
           good: null,
         }
       },
       hiddenInput: {
-        points: 0,
+        points: 5,
         reasons: {
-          bad: null,
+          bad: [
+            "Found 2 hidden inputs on the page. While hidden inputs are commonly used for legitimate purposes, such as storing necessary data, they can also be exploited by scam sites to track tokens or collect sensitive information."
+          ],
           good: null,
         }
       }
     },
-    riskLevel: "LOW",
+    riskLevel: "MODERATE",
     screenshots: [
-      { image: "https://ik.imagekit.io/iakg6rt33o/20241224_040959_1735013399676242375?updatedAt=1735013401363" },
-      { image: "https://ik.imagekit.io/iakg6rt33o/20241224_040954_1735013394422837181?updatedAt=1735013396657" },
+      { image: "20241225_060709_1735106829222268668" },
+      { image: "20241225_060714_1735106834440804806" },
+      { image: "20241225_060719_1735106839334431604" }
     ]
   }
 }
-
