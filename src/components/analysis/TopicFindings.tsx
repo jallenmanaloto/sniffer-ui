1e38800210401b6df2b80911bad7b3fa850b5e46
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 14:30:19 +0800

    feat: map each topics

--- src/components/analysis/TopicFindings.tsx
+++ src/components/analysis/TopicFindings.tsx
@@ -3,7 +3,7 @@ import { Circle, CircleAlert } from "lucide-react";
 
 const Finding = ({ topic, reasons }: { topic: string, reasons: Reasons }) => {
   return (
-    <div>
+    <div className="py-5">
       <h2 className="text-lg text-primary/80 font-semibold tracking-wider py-2">
         {topic}
       </h2>
@@ -11,13 +11,13 @@ const Finding = ({ topic, reasons }: { topic: string, reasons: Reasons }) => {
       {/** Mapping of Good Reasons */}
       {reasons.good !== null ? reasons.good.map((reason, key: number) => {
         return (
-          <div key={key} className="reason-container flex items-center gap-2">
+          <div key={key} className="reason-container flex justify-between items-start gap-2 py-2">
             <Circle
-              className="text-teal-600 dark:text-teal-500/75"
+              className="text-teal-600 dark:text-teal-500/75 flex-shrink mt-1"
               size={14}
               strokeWidth={4}
             />
-            <p>
+            <p className="w-5/6 flex-grow">
               {reason}
             </p>
           </div>
@@ -27,13 +27,13 @@ const Finding = ({ topic, reasons }: { topic: string, reasons: Reasons }) => {
       {/** Mapping of Bad Reasons */}
       {reasons.bad !== null ? reasons.bad.map((reason, key: number) => {
         return (
-          <div key={key} className="reason-container flex items-center gap-2">
+          <div key={key} className="reason-container flex justify-between items-start gap-2 py-2">
             <CircleAlert
-              className="text-red-600 dark:text-red-500/75"
+              className="text-red-600 dark:text-red-500/75 flex-shrink mt-1"
               size={14}
               strokeWidth={4}
             />
-            <p>
+            <p className="w-5/6 flex-grow">
               {reason}
             </p>
           </div>
@@ -51,7 +51,29 @@ export default function TopicFindings({ topicFinding }: { topicFinding: TopicFin
           Findings
         </h1>
       </div>
-      <Finding topic="Domain" reasons={topicFinding.domain.reasons} />
+      {/** Domain Topic */}
+      {topicFinding.domain !== null
+        ? <Finding topic="Domain" reasons={topicFinding.domain.reasons} />
+        : null
+      }
+
+      {/** Content Topic */}
+      {topicFinding.content !== null
+        ? <Finding topic="Content" reasons={topicFinding.content.reasons} />
+        : null
+      }
+
+      {/** Certificate Topic */}
+      {topicFinding.certificate !== null
+        ? <Finding topic="Certificate" reasons={topicFinding.certificate.reasons} />
+        : null
+      }
+
+      {/** Hidden Inputs Topic */}
+      {topicFinding.hiddenInput !== null
+        ? <Finding topic="Hidden Inputs" reasons={topicFinding.hiddenInput.reasons} />
+        : null
+      }
     </div>
   )
 }
