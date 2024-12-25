5e7b374871608504c3bc11df747d3b8a22ce64fd
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 17:31:16 +0800

    feat: add terms of use page on footer link

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,17 +11,21 @@ export default function Footer() {
       name: "Privacy Policy",
       route: "/privacy-policy"
     },
+    {
+      name: "Terms of Use",
+      route: "/terms-of-use"
+    },
   ];
   return (
     <footer className="border-t-2 py-8">
       <div className="footer-wrapper container mx-auto px-8 lg:px-36">
         <div className="quick-links">
           <h3 className="text-base font-semibold">Quick Links</h3>
-          <div className="links flex flex-col gap-4">
+          <div className="links flex flex-col py-2">
             {links.map((link: Link, idx: number) => {
               return (
                 <Link key={idx} href={link.route}>
-                  <h4 className="py-2 text-primary/80 cursor-pointer">{link.name}</h4>
+                  <h4 className="text-primary/80 cursor-pointer">{link.name}</h4>
                 </Link>
               )
             })}
