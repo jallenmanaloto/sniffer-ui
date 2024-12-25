756db74f9f3f586719b80afc8fb84a7ff58ce83f
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 16:15:46 +0800

    feat: add Disclaimer component on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Analysis from "@/components/analysis";
+import Disclaimer from "@/components/disclaimer";
 import Header from "@/components/header"
 import Lookup from "@/components/lookup/Lookup";
 import { responseData } from "@/data/mock";
@@ -6,8 +7,9 @@ import { responseData } from "@/data/mock";
 export default function Home() {
   return (
     <div className="w-screen dark:bg-slate-900">
+      <Header />
       <main className="container mx-auto px-8 lg:px-36">
-        <Header />
+        <Disclaimer />
         <Lookup />
         <Analysis response={responseData} />
       </main>
