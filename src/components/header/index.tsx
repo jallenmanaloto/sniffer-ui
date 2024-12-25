642d6209ca07a5c8740755c0578c35ce9bcbab8f
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 16:15:35 +0800

    feat: add border and adjust paddings

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,13 +2,15 @@ import DarkMode from "./DarkMode";
 
 export default function Header() {
   return (
-    <header>
-      <div className="flex items-center justify-between py-4">
-        <h1 className="text-4xl font-bold dark:text-teal-500/75">
-          BaitSniffer
-        </h1>
-        <div>
-          <DarkMode />
+    <header className="border-b-2">
+      <div className="header-wrapper container mx-auto px-8 lg:px-36">
+        <div className="flex items-center justify-between py-4">
+          <h1 className="text-4xl font-bold dark:text-teal-500/75">
+            BaitSniffer
+          </h1>
+          <div>
+            <DarkMode />
+          </div>
         </div>
       </div>
     </header>
