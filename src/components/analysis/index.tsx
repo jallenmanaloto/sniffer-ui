58830faf83d4624ffa2d796e8a22d700db90017e
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 14:31:46 +0800

    refactor: remove unnecessary vars

--- src/components/analysis/index.tsx
+++ src/components/analysis/index.tsx
@@ -1,21 +1,14 @@
-import { RiskLevel, Screenshot } from "@/lib/types";
+import { AnalysisResponse, RiskLevel } from "@/lib/types";
 import Screenshots from "./Screenshots";
-import { responseData } from "@/data/mock";
 import Summary from "./Summary";
 import TopicFindings from "./TopicFindings";
 
-export default function Analysis() {
-  // const { riskLevel, screenshots, topicFinding } = findings;
-  const { findings, databaseMatch } = responseData;
-
-  const screenshots: Screenshot[] = [
-    { image: "https://ik.imagekit.io/iakg6rt33o/20241224_040959_1735013399676242375?updatedAt=1735013401363" },
-    { image: "https://ik.imagekit.io/iakg6rt33o/20241224_040954_1735013394422837181?updatedAt=1735013396657" },
-  ]
+export default function Analysis({ response }: { response: AnalysisResponse }) {
+  const { findings } = response;
   return (
-    <div className="">
+    <div>
       <Summary riskLevel={findings.riskLevel as RiskLevel} />
-      <Screenshots screenshots={screenshots} />
+      <Screenshots screenshots={findings.screenshots} />
       <TopicFindings topicFinding={findings.topicFinding} />
     </div>
   )
