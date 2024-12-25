56b317f21dbe98d3aac41daeb7850136b1953eee
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-25 16:15:21 +0800

    style: add padding-y for analysis component

--- src/components/analysis/index.tsx
+++ src/components/analysis/index.tsx
@@ -6,7 +6,7 @@ import TopicFindings from "./TopicFindings";
 export default function Analysis({ response }: { response: AnalysisResponse }) {
   const { findings } = response;
   return (
-    <div>
+    <div className="py-12">
       <Summary riskLevel={findings.riskLevel as RiskLevel} />
       <Screenshots screenshots={findings.screenshots} />
       <TopicFindings topicFinding={findings.topicFinding} />
