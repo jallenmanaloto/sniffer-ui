a6f3e45f5d90ee24c89ac285cc773c6c668b5fbc
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 15:42:10 +0800

    feat: add healthcheck

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
+"use client"
+
 import Analysis from "@/components/analysis";
 import Disclaimer from "@/components/disclaimer";
 import Footer from "@/components/footer";
 import Header from "@/components/header"
 import Lookup from "@/components/lookup/Lookup";
 import { responseData } from "@/data/mock";
+import { useHealthCheck } from "@/data/api/hooks";
 
 export default function Home() {
+  const { data: health } = useHealthCheck();
+
   return (
     <div className="w-screen dark:bg-slate-900">
       <Header />
       <main className="container mx-auto px-8 lg:px-36">
         <Disclaimer />
-        <Lookup />
+        <Lookup available={health} />
         <Analysis response={responseData} />
       </main>
       <Footer />
