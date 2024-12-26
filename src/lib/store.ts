5b16189e4cb15cc0abb2c84a4ae6908bd518a28d
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 15:18:11 +0800

    feat: create service store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -10,3 +10,13 @@ export const useAnalysis = create<TAnalysisStore>((set) => ({
   analysisResponse: null,
   setAnalysisResponse: (analysisResponse: AnalysisResponse) => set({ analysisResponse: analysisResponse }),
 }));
+
+export type TServiceStore = {
+  available: boolean;
+  setAvailable: (available: boolean) => void;
+}
+
+export const useService = create<TServiceStore>((set) => ({
+  available: true,
+  setAvailable: (available: boolean) => set({ available: available }),
+}));
