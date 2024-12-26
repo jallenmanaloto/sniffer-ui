ecd0bb833a058c9aa04e6387d26324d20bcd8ea4
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 15:41:25 +0800

    feat: apply button disable on health status

--- src/components/lookup/Lookup.tsx
+++ src/components/lookup/Lookup.tsx
@@ -1,14 +1,17 @@
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
-export default function Lookup() {
+export default function Lookup({ available }: { available: boolean | undefined }) {
   return (
     <div className="lookup-container grid grid-cols-4 gap-y-3 gap-x-5">
       <Input
         className="col-span-4 sm:col-span-3 border-gray-300"
         placeholder="Paste URL here"
       />
-      <Button className="col-span-4 sm:col-span-1 dark:bg-teal-500/75 dark:hover:bg-teal-700 dark:text-slate-200">
+      <Button
+        className="col-span-4 sm:col-span-1 dark:bg-teal-500/75 dark:hover:bg-teal-700 dark:text-slate-200"
+        disabled={available}
+      >
         <h2 className="text-lg">
           Lookup
         </h2>
