9090986acde1aea1c1bee313a7e1cd943ec4fd82
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 15:59:54 +0800

    feat: add message if service is available

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,18 @@ import Lookup from "@/components/lookup/Lookup";
 import { responseData } from "@/data/mock";
 import { useHealthCheck } from "@/data/api/hooks";
 import { useAnalysis } from "@/lib/store";
+import { Info } from "lucide-react";
+
+function UnavailableService() {
+  return (
+    <div className="flex gap-2 py-5">
+      <Info className="text-red-200 h-5 w-5" />
+      <p className="text-red-200">
+        This service is currently not available.
+      </p>
+    </div>
+  )
+}
 
 export default function Home() {
   const { data: health } = useHealthCheck();
@@ -19,6 +31,7 @@ export default function Home() {
       <main className="container flex-grow mx-auto px-8 lg:px-36">
         <Disclaimer />
         <Lookup available={health} />
+        {health ? <UnavailableService /> : null}
         {analysisResponse === null ? null : <Analysis response={responseData} />}
       </main>
       <Footer />
