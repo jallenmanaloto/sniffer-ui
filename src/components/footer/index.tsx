a1cc54e194236945925243834783a02778faa00c
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 15:52:55 +0800

    style: rearrange positioning

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -21,22 +21,24 @@ export default function Footer() {
     },
   ];
   return (
-    <footer className="border-t-2 py-8">
+    <footer className="border-t-2 py-4">
       <div className="footer-wrapper container mx-auto px-8 lg:px-36">
-        <div className="quick-links">
-          <h3 className="text-base font-semibold">Quick Links</h3>
-          <div className="links flex flex-col py-2">
-            {links.map((link: Link, idx: number) => {
-              return (
-                <Link key={idx} href={link.route}>
-                  <h4 className="text-primary/80 cursor-pointer">{link.name}</h4>
-                </Link>
-              )
-            })}
+        <div className="sm:flex sm:gap-x-32 sm:justify-center sm:items-center">
+          <div className="quick-links">
+            <h3 className="text-base font-semibold">Quick Links</h3>
+            <div className="links flex flex-col py-2">
+              {links.map((link: Link, idx: number) => {
+                return (
+                  <Link key={idx} href={link.route}>
+                    <h4 className="text-primary/80 cursor-pointer">{link.name}</h4>
+                  </Link>
+                )
+              })}
+            </div>
           </div>
-        </div>
-        <div className="copyright flex justify-center py-3">
-          <h4>© BaitSniffer 2024</h4>
+          <div className="copyright flex sm:justify-center py-3">
+            <h4>© BaitSniffer 2024</h4>
+          </div>
         </div>
       </div>
     </footer>
