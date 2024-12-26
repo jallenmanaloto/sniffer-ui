fd22f1cf8c9f66a50514d27558a6ca3bbf8476fb
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 16:57:11 +0800

    feat: add check if currently analyzing

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,12 +3,16 @@ import { AnalysisResponse } from "./types";
 
 export type TAnalysisStore = {
   analysisResponse: AnalysisResponse | null;
+  isAnalyzing: boolean;
   setAnalysisResponse: (analysisResponse: AnalysisResponse) => void;
+  setIsAnalyzing: (isAnalyzing: boolean) => void;
 }
 
 export const useAnalysis = create<TAnalysisStore>((set) => ({
   analysisResponse: null,
+  isAnalyzing: false,
   setAnalysisResponse: (analysisResponse: AnalysisResponse) => set({ analysisResponse: analysisResponse }),
+  setIsAnalyzing: (isAnalyzing: boolean) => set({ isAnalyzing: isAnalyzing }),
 }));
 
 export type TServiceStore = {
