2fe05ac5f9875744dc9933d73b63f007b5de1d2c
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 15:46:41 +0800

    style: use flex-grow on main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,19 @@ import Header from "@/components/header"
 import Lookup from "@/components/lookup/Lookup";
 import { responseData } from "@/data/mock";
 import { useHealthCheck } from "@/data/api/hooks";
+import { useAnalysis } from "@/lib/store";
 
 export default function Home() {
   const { data: health } = useHealthCheck();
+  const { analysisResponse } = useAnalysis();
 
   return (
-    <div className="w-screen dark:bg-slate-900">
+    <div className="flex flex-col min-h-screen w-screen dark:bg-slate-900">
       <Header />
-      <main className="container mx-auto px-8 lg:px-36">
+      <main className="container flex-grow mx-auto px-8 lg:px-36">
         <Disclaimer />
         <Lookup available={health} />
-        <Analysis response={responseData} />
+        {analysisResponse === null ? null : <Analysis response={responseData} />}
       </main>
       <Footer />
     </div>
