3960bdaf62cd9be4bcead76ea726cfcc0bd45b75
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-26 22:45:33 +0800

    style: shorten the width of disclaimer and lookup

--- src/components/disclaimer/index.tsx
+++ src/components/disclaimer/index.tsx
@@ -13,26 +13,28 @@ export default function Disclaimer() {
   const readingDisclaimer = "While this app provides an analysis of whether a website is likely to be a scam, it should not be relied upon as the sole method for determining the safety of a website. Always exercise caution and verify links from trusted sources before interacting with them.";
 
   return (
-    <div className="py-6">
-      <div className="block md:hidden" onClick={() => setReadDisclaimer(!readDisclaimer)}>
-        <Alert>
-          <CircleAlert className="h-4 w-4" />
-          <AlertTitle className="font-bold">
-            {readDisclaimer ? "Disclaimer" : "Click to read disclaimer"}
-          </AlertTitle>
-          <AlertDescription className={`h-auto md:h-full tracking-wide leading-5`}>
-            {readDisclaimer ? readingDisclaimer : ""}
-          </AlertDescription>
-        </Alert>
-      </div>
-      <div className="hidden md:block">
-        <Alert>
-          <CircleAlert className="h-4 w-4" />
-          <AlertTitle className="font-bold">Disclaimer</AlertTitle>
-          <AlertDescription className="md:h-full tracking-wide leading-5">
-            While this app provides an analysis of whether a website is likely to be a scam, it should not be relied upon as the sole method for determining the safety of a website. Always exercise caution and verify links from trusted sources before interacting with them.
-          </AlertDescription>
-        </Alert>
+    <div className="lg:flex lg:justify-center">
+      <div className="py-6 lg:w-3/4">
+        <div className="block md:hidden" onClick={() => setReadDisclaimer(!readDisclaimer)}>
+          <Alert>
+            <CircleAlert className="h-4 w-4" />
+            <AlertTitle className="font-bold">
+              {readDisclaimer ? "Disclaimer" : "Click to read disclaimer"}
+            </AlertTitle>
+            <AlertDescription className={`h-auto md:h-full tracking-wide leading-5`}>
+              {readDisclaimer ? readingDisclaimer : ""}
+            </AlertDescription>
+          </Alert>
+        </div>
+        <div className="hidden md:block">
+          <Alert>
+            <CircleAlert className="h-4 w-4" />
+            <AlertTitle className="font-bold">Disclaimer</AlertTitle>
+            <AlertDescription className="md:h-full tracking-wide leading-5">
+              While this app provides an analysis of whether a website is likely to be a scam, it should not be relied upon as the sole method for determining the safety of a website. Always exercise caution and verify links from trusted sources before interacting with them.
+            </AlertDescription>
+          </Alert>
+        </div>
       </div>
     </div>
   )
--- src/components/lookup/Lookup.tsx
+++ src/components/lookup/Lookup.tsx
@@ -3,19 +3,21 @@ import { Input } from "../ui/input";
 
 export default function Lookup({ available }: { available: boolean | undefined }) {
   return (
-    <div className="lookup-container grid grid-cols-4 gap-y-3 gap-x-5">
-      <Input
-        className="col-span-4 sm:col-span-3 border-gray-300"
-        placeholder="Paste URL here"
-      />
-      <Button
-        className="col-span-4 sm:col-span-1 dark:bg-teal-500/75 dark:hover:bg-teal-700 dark:text-slate-200"
-        disabled={available}
-      >
-        <h2 className="text-lg">
-          Lookup
-        </h2>
-      </Button>
+    <div className="lg:flex lg:justify-center">
+      <div className="lookup-container grid grid-cols-4 gap-y-3 gap-x-5 lg:w-3/4">
+        <Input
+          className="col-span-4 sm:col-span-3 border-gray-300"
+          placeholder="Paste URL here"
+        />
+        <Button
+          className="col-span-4 sm:col-span-1 dark:bg-teal-500/75 dark:hover:bg-teal-700 dark:text-slate-200"
+          disabled={available}
+        >
+          <h2 className="text-lg">
+            Lookup
+          </h2>
+        </Button>
+      </div>
     </div>
   )
 }
