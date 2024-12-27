911e556eef646d41b5de375c296b69c12e5da4cd
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-27 14:22:59 +0800

    feat: return the data from the mutation

--- src/data/api/hooks.ts
+++ src/data/api/hooks.ts
@@ -1,19 +1,12 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { analyze, healthCheck } from "./api";
-import { useAnalysis } from "@/lib/store";
-import { AnalysisResponse } from "@/lib/types";
 
 export function useAnalyze() {
   return useMutation({
-    mutationFn: async (urls: string[]) => analyze(urls),
-    mutationKey: ["analyze"],
-    onSuccess: async (data: AnalysisResponse) => {
-      const { setAnalysisResponse } = useAnalysis();
-      setAnalysisResponse(data);
+    mutationFn: async (urls: string[]) => {
+      return analyze(urls);
     },
-    onError: () => {
-      console.error("Error analyzing",)
-    }
+    mutationKey: ["analyze"],
   });
 }
 
