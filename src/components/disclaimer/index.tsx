6e5d3bd5400657c687ae08d0e1e456d52d11599d
jallenmanaloto <jallen.manaloto@gmail.com>
2024-12-27 12:36:45 +0800

    style: update text color to slate

--- src/components/disclaimer/index.tsx
+++ src/components/disclaimer/index.tsx
@@ -29,8 +29,8 @@ export default function Disclaimer() {
         <div className="hidden md:block">
           <Alert>
             <CircleAlert className="h-4 w-4" />
-            <AlertTitle className="font-bold">Disclaimer</AlertTitle>
-            <AlertDescription className="md:h-full tracking-wide leading-5">
+            <AlertTitle className="font-bold dark:text-slate-100/90">Disclaimer</AlertTitle>
+            <AlertDescription className="md:h-full tracking-wide leading-5 dark:text-slate-100/70">
               While this app provides an analysis of whether a website is likely to be a scam, it should not be relied upon as the sole method for determining the safety of a website. Always exercise caution and verify links from trusted sources before interacting with them.
             </AlertDescription>
           </Alert>
